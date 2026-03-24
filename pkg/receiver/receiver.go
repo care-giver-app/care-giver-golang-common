45f6c6de998cb6efa4f3c8c8f74941f3d2437a80
Trevor <twilliams0095@gmail.com>
2026-03-23 20:32:30 -0500

    wip updating receiver

--- pkg/receiver/receiver.go
+++ pkg/receiver/receiver.go
@@ -12,15 +12,30 @@ const (
 )
 
 type Receiver struct {
-	ReceiverID string `json:"receiverId" dynamodbav:"receiver_id"`
-	FirstName  string `json:"firstName" dynamodbav:"first_name"`
-	LastName   string `json:"lastName" dynamodbav:"last_name"`
+	ReceiverID     string   `json:"receiverId" dynamodbav:"receiver_id"`
+	FirstName      string   `json:"firstName" dynamodbav:"first_name"`
+	LastName       string   `json:"lastName" dynamodbav:"last_name"`
+	EventsTracking []string `json:"eventsTracking" dynamodbav:"events_tracking"`
 }
 
-func NewReceiver(firstName string, lastName string) *Receiver {
-	return &Receiver{
+type ReceiverOption func(*Receiver)
+
+func WithEventsTracking(events []string) ReceiverOption {
+	return func(r *Receiver) {
+		r.EventsTracking = events
+	}
+}
+
+func NewReceiver(firstName string, lastName string, opts ...ReceiverOption) *Receiver {
+	r := &Receiver{
 		ReceiverID: fmt.Sprintf("%s#%s", DBPrefix, uuid.New()),
 		FirstName:  firstName,
 		LastName:   lastName,
 	}
+
+	for _, opt := range opts {
+		opt(r)
+	}
+
+	return r
 }
